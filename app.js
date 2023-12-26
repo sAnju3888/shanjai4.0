79a33518ad9630fc98fecd5227cdea1665f3a5d6
shanjai <114935971+sAnju3888@users.noreply.github.com>
2023-12-26 16:09:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-import express from 'express'
-import bodyParser from 'body-parser';
-import { getNotes, getNote , createPost} from './models/database.js';
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT);
-app.use(express.static("public"))
-app.set('view engine','ejs');
+
+const { createPost, getNotes } = require('./models/database.js');
+ var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -36,7 +49,7 @@ app.post('/submit',async(req,res)=>{
 
 
 app.get('/view-all',async (req,res)=>{
-    const notes = await getNotes()
+    const notes = await getNotes();
     res.render('display',{notes:JSON.stringify(notes)});
 });
 
@@ -45,7 +58,7 @@ app.get('/edit',(req,res)=>{
 });
 
 app.get('/blogs',async (req,res)=>{
-    const notes = await getNotes()
+    const notes = await getNotes();
     res.render('display',{notes:JSON.stringify(notes)});
 
 });
@@ -53,7 +66,7 @@ app.get('/blogs',async (req,res)=>{
 
 app.get('/notes',async (req,res)=>{
     const raw = req.query.raw;
-    const notes = await getNotes()
+    const notes = await getNotes();
     //res.send(notes);
     if(raw){
         res.send(notes);
@@ -66,7 +79,7 @@ app.get('/notes',async (req,res)=>{
 
 app.get('/notes/:id',async (req,res)=>{
     const id = req.params.id;
-    const note = await getNotes(id)
+    const note = await getNotes(id);
     res.send(note);
 })
 
@@ -75,3 +88,21 @@ app.use((err,req,res,next) =>{
     console.error(err.stack);
     res.status(500).send('Something went wrong');
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
