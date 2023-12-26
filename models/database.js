4f5d1cd7b0e94c0b7f97631689b8e08efd4ee018
shanjai <114935971+sAnju3888@users.noreply.github.com>
2023-12-26 16:10:11 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,62 +1,60 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-
-dotenv.config();
-  
-const pool = mysql.createPool({
-       host:process.env.MYSQL_HOST,
-       user:process.env.MYSQL_USER,
-       password:process.env.MYSQL_PASSWORD,
-       database:process.env.MYSQL_DATABASE,
-    }).promise()
+const mysql = require('mysql2'); 
 
 
+// const connection = mysql.createConnection({
+//     host: '34.133.155.116',
+//     user: 'root',
+//     password: 'root',
+//     database: 'blog',
+// }).promise();
 
-    
-// Fetching columns in a table
-export async function getNotes() {
-    const [rows] = await pool.query('SELECT * FROM my_posts');
-    return rows;
-}
+const connection = mysql.createPool({
+    // host:process.env.MYSQL_HOST,
+    // user:process.env.MYSQL_USER,
+    // password:process.env.MYSQL_PASSWORD,
+    // database:process.env.MYSQL_DATABASE,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'blog',
+ }).promise()
 
-export async function getId() {
-    const [rows] = await pool.query('SELECT id FROM my_posts');
-    return rows;
-}
+const getNotes = async function getNotes() {
+    try {
+        const [rows] = await connection.query('SELECT * FROM my_posts');
+        return rows;
+    } catch (error) {
+        console.error('Error executing getNotes query:', error);
+        throw error;
+    }
+};
 
-export async function getName() {
-    const [rows] = await pool.query('SELECT topicName FROM my_posts');
-    return rows;
-}
+const createPost = async function createPost(topicName, topicHeading, topicContent) {
+    try {
+        const [result] = await connection.query(`
+            INSERT INTO my_posts(topicName, topicHeading, topicContent)
+            VALUES(?,?,?)`, [topicName, topicHeading, topicContent]);
 
-export async function getHeading() {
-    const [rows] = await pool.query('SELECT topicHeading FROM my_posts');
-    return rows;
-}
+        const id = result.insertId;
+        return getNote(id);
+    } catch (error) {
+        console.error('Error executing createPost query:', error);
+        throw error;
+    }
+};
 
-export async function getContent() {
-    const [rows] = await pool.query('SELECT topicContent FROM my_posts');
-    return rows;
-}
+const getNote = async function getNote(id) {
+    try {
+        const [rows] = await connection.query('SELECT * FROM my_posts WHERE id=?', [id]);
+        return rows[0];
+    } catch (error) {
+        console.error('Error executing getNote query:', error);
+        throw error;
+    }
+};
 
-// selecting based on a condition
-export async function getNote(id){
-    const [rows] = await pool.query('SELECT * FROM my_posts WHERE id=?',[id]);
-    return rows[0];
-}
-
-
-// Inserting a new row into a table
-export async function createPost(topicName, topicHeading, topicContent){
-   const [result] =  await pool.query(`
-    INSERT INTO my_posts(topicName, topicHeading, topicContent)
-    VALUES(?,?,?)`,[topicName, topicHeading, topicContent]);
-    
-    const id = result.insertId
-    return getNote(id);
-}
-
-
-
-
-
+module.exports = {
+    createPost,
+    getNotes,
+    getNote
+};
